d8775c8e6654b78a7b976254f6e02326f00217f7
mangizadd <nikolay.d@mail.com>
2022-09-23 19:00:30 +0300

    edit

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
-const express = require('express')
-const hbs = require('express-handlebars').create({
-    extname: '.hbs'
-})
+const express = require("express");
+const hbs = require("express-handlebars").create({
+    extname: ".hbs",
+});
 
-const homeControler = require('./controlers/homeControler')
-const catalogControler = require('./controlers/catalogControler')
+const homeControler = require("./controlers/homeControler");
+const catalogControler = require("./controlers/catalogControler");
 
-const errorControler = require('./controlers/errorControler')
+const errorControler = require("./controlers/errorControler");
 
-const app = express()
-app.engine('.hbs', hbs.engine)
-app.set('view engine', '.hbs')
-app.use(express.urlencoded({ extended: false }))
-app.use('/static', express.static('static'))
+const app = express();
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+app.use(express.urlencoded({ extended: false }));
+app.use("/static", express.static("static"));
 
-app.use(homeControler)
-app.use('/about', homeControler)
-app.use('/catalog', catalogControler)
-app.use('/catalog/:id', catalogControler)
+//routers
+app.use(homeControler);
+app.use("/about", homeControler);
+app.use("/catalog", catalogControler);
+app.use("/catalog/:id", catalogControler);
 
+app.all("*", errorControler);
 
-
-app.all('*', errorControler)
-
-
-
-
-
-app.listen(3000, () => console.log('Server started on port 3000'))
-
-
+app.listen(3000, () => console.log("Server started on port 3000"));
